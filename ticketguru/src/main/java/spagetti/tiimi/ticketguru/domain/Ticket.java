b5cb236eb4f8753da0db9d31ac2c23c54bdb7d79
Janne <bhi256@myy.haaga-helia.fi>
2025-09-05 17:19:46 +0300

    Added basic ticket class

---
+++ ticketguru/src/main/java/spagetti/tiimi/ticketguru/domain/Ticket.java
@@ -0,0 +1,29 @@
+package spagetti.tiimi.ticketguru.domain;
+
+public class Ticket {
+
+    private String name;
+
+    public Ticket () {
+
+    }
+
+    public Ticket (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+}
