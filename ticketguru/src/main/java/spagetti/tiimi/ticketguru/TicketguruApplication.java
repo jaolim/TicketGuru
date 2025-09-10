fb6d571b8467a55a4eb5a291cd291b5e3b037614
Janne <bhi256@myy.haaga-helia.fi>
2025-09-10 17:08:53 +0300

    Poistettu turhaa uloskommentoitua koodia

--- ticketguru/src/main/java/spagetti/tiimi/ticketguru/TicketguruApplication.java
+++ ticketguru/src/main/java/spagetti/tiimi/ticketguru/TicketguruApplication.java
@@ -36,21 +36,3 @@ public class TicketguruApplication {
 	}
 
 }
-
-/*
- * @Bean
- * public CommandLineRunner ticketGuru(TicketRepository repository) {
- * return (args) -> {
- * log.info("Adding some test books");
- * repository.save(new Ticket("test1"));
- * repository.save(new Ticket("Test 2"));
- * repository.save(new Ticket("Test 3"));
- * repository.save(new Ticket("Test 3"));
- * log.info("fetch tickets");
- * for (Ticket ticket : repository.findAll()) {
- * log.info(ticket.toString());
- * }
- * 
- * };
- * }
- */
