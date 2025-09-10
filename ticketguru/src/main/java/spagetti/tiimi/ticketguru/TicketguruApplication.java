55c9cf4919f399269ad9a010796d6770b3299380
Janne <bhi256@myy.haaga-helia.fi>
2025-09-10 16:56:50 +0300

    H2 db ja TicketRepository lisatty testausta varten

--- ticketguru/src/main/java/spagetti/tiimi/ticketguru/TicketguruApplication.java
+++ ticketguru/src/main/java/spagetti/tiimi/ticketguru/TicketguruApplication.java
@@ -1,13 +1,56 @@
 package spagetti.tiimi.ticketguru;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import spagetti.tiimi.ticketguru.domain.Ticket;
+import spagetti.tiimi.ticketguru.domain.TicketRepository;
 
 @SpringBootApplication
 public class TicketguruApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(TicketguruApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketguruApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner ticketGuru(TicketRepository repository) {
+		return (args) -> {
+			log.info("Adding some test books");
+			repository.save(new Ticket("test1"));
+			repository.save(new Ticket("Test 2"));
+			repository.save(new Ticket("Test 3"));
+			repository.save(new Ticket("Test 3"));
+			log.info("fetch tickets");
+			for (Ticket ticket : repository.findAll()) {
+				log.info(ticket.toString());
+			}
+
+		};
+	}
+
 }
+
+/*
+ * @Bean
+ * public CommandLineRunner ticketGuru(TicketRepository repository) {
+ * return (args) -> {
+ * log.info("Adding some test books");
+ * repository.save(new Ticket("test1"));
+ * repository.save(new Ticket("Test 2"));
+ * repository.save(new Ticket("Test 3"));
+ * repository.save(new Ticket("Test 3"));
+ * log.info("fetch tickets");
+ * for (Ticket ticket : repository.findAll()) {
+ * log.info(ticket.toString());
+ * }
+ * 
+ * };
+ * }
+ */
--- ticketguru/src/main/java/spagetti/tiimi/ticketguru/web/HomeController.java
+++ ticketguru/src/main/java/spagetti/tiimi/ticketguru/web/HomeController.java
@@ -5,15 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 import spagetti.tiimi.ticketguru.domain.Ticket;
+import spagetti.tiimi.ticketguru.domain.TicketRepository;
 
 @Controller
 public class HomeController {
+
+    private TicketRepository repository;
+
+    public HomeController(TicketRepository repository) {
+        this.repository = repository;
+    }
 
     public static Ticket testTicket = new Ticket("Testi");
 
     @GetMapping(value = {"/", "/index"})
     public String getIndex(Model model) {
         model.addAttribute("ticket", testTicket);
+        model.addAttribute("tickets", repository.findAll());
         return "index";
     }
 
