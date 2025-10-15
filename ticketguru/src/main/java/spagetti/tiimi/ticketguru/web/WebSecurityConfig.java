0cadc086e6947df513f91c6410e1c39740eb55e1
Janne <bhi256@myy.haaga-helia.fi>
2025-10-15 13:12:07 +0300

    BasicAuth enabloitu kaikkialle ilman roolimäärityksiä

--- ticketguru/src/main/java/spagetti/tiimi/ticketguru/web/WebSecurityConfig.java
+++ ticketguru/src/main/java/spagetti/tiimi/ticketguru/web/WebSecurityConfig.java
@@ -2,31 +2,36 @@ package spagetti.tiimi.ticketguru.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration 
+@Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() { 
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+ 
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/", "/home", "/css/**", "/login").permitAll())
+                .requestMatchers("/", "/home", "/css/**", "/login", "/*/*", "/*").permitAll())
                 .formLogin(formlogin -> formlogin
                         .defaultSuccessUrl("/", true)
                         .permitAll())
+                .httpBasic(Customizer.withDefaults())
                 .logout(logout -> logout
                         .permitAll())
                 .csrf(csrf -> csrf.disable());
+    
         return http.build();
+
     }
 
 }
