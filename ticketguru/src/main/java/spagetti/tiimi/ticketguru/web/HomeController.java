6922bf64879fddc07d4a31f0f6bfc7e07ea2e4ac
Janne <bhi256@myy.haaga-helia.fi>
2025-10-28 20:29:33 +0200

    Poistettu testausta varten tehdyt ajo-aikaiset oliot ja poistettu käyttäjien listaus etusivulta

--- ticketguru/src/main/java/spagetti/tiimi/ticketguru/web/HomeController.java
+++ ticketguru/src/main/java/spagetti/tiimi/ticketguru/web/HomeController.java
@@ -1,6 +1,6 @@
 package spagetti.tiimi.ticketguru.web;
 
-import java.time.LocalDateTime;
+//import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 import spagetti.tiimi.ticketguru.domain.EventRepository;
-import spagetti.tiimi.ticketguru.domain.Sale;
+//import spagetti.tiimi.ticketguru.domain.Sale;
 import spagetti.tiimi.ticketguru.domain.SaleRepository;
-import spagetti.tiimi.ticketguru.domain.Ticket;
-import spagetti.tiimi.ticketguru.domain.Cost;
+//import spagetti.tiimi.ticketguru.domain.Ticket;
+//import spagetti.tiimi.ticketguru.domain.Cost;
 import spagetti.tiimi.ticketguru.domain.CostRepository;
-import spagetti.tiimi.ticketguru.domain.Event;
-import spagetti.tiimi.ticketguru.domain.AppUser;
+//import spagetti.tiimi.ticketguru.domain.Event;
+//import spagetti.tiimi.ticketguru.domain.AppUser;
 import spagetti.tiimi.ticketguru.domain.TicketRepository;
-import spagetti.tiimi.ticketguru.domain.TicketType;
+//import spagetti.tiimi.ticketguru.domain.TicketType;
 import spagetti.tiimi.ticketguru.domain.TicketTypeRepository;
-import spagetti.tiimi.ticketguru.domain.AppUserRepository;
+//import spagetti.tiimi.ticketguru.domain.AppUserRepository;
 
 
 @Controller
@@ -28,8 +28,8 @@ public class HomeController {
     private TicketRepository repository;
     @Autowired
     private EventRepository erepository;
-    @Autowired
-    private AppUserRepository urepository;
+    //@Autowired
+    //private AppUserRepository urepository;
     @Autowired
     private CostRepository crepository;
     @Autowired
@@ -38,27 +38,33 @@ public class HomeController {
     private SaleRepository srepository;
 
 
-
+ 
+    /* 
     public static Event testEvent = new Event("Testitapahtuma", "Testipaikka", LocalDateTime.now());
     public static AppUser testUser = new AppUser("User2", "$2a$10$aY3WHmE3fYMoM.vxVCqv.Oe3dwUu8ha5CvkazPOk0698xq/9kxfkC","Testi", "Esimerkki", "USER");
     public static TicketType testType = new TicketType("Lapsi");
     public static Cost testCost = new Cost(testType, 9.99, testEvent);
     public static Ticket testTicket = new Ticket(testCost);
     public static Sale testSale = new Sale(testUser, LocalDateTime.now());
+    */
+    
 
     @GetMapping(value = {"/", "/index"})
     public String getIndex(Model model) {
+        /* 
         model.addAttribute("ticket", testTicket);
         model.addAttribute("event", testEvent);
         model.addAttribute("user", testUser);
         model.addAttribute("sale", testSale);
         model.addAttribute("tickettype", testType);
+        */
+        /* 
+        model.addAttribute("cost", testCost);
+*/
         model.addAttribute("tickets", repository.findAll());
         model.addAttribute("events", erepository.findAll());
-        model.addAttribute("users", urepository.findAll());
         model.addAttribute("sales", srepository.findAll());
         model.addAttribute("tickettypes", trepository.findAll());
-        model.addAttribute("cost", testCost);
         model.addAttribute("tickets", repository.findAll());
         model.addAttribute("events", erepository.findAll());
         model.addAttribute("costs", crepository.findAll());
