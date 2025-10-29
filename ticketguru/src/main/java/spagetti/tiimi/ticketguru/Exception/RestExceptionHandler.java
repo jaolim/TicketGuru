98830f280d87d48425f3773e81f111f1ca8a2f75
Jenny <205511984+jenny492@users.noreply.github.com>
2025-10-29 18:30:18 +0200

    Lisätty ticketille endpoint patch ticket/{id} ja poikkeus ticketAlreadyRedeemed

--- ticketguru/src/main/java/spagetti/tiimi/ticketguru/Exception/RestExceptionHandler.java
+++ ticketguru/src/main/java/spagetti/tiimi/ticketguru/Exception/RestExceptionHandler.java
@@ -13,53 +13,62 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<Map<String, Object>> RestBadRequest(BadRequestException exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Map.of(
-                        "status", HttpStatus.BAD_REQUEST.value(),
-                        "error", "Bad Request",
-                        "message", exception.getMessage()));
-
-    }
-
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Map<String, Object>> RestNotFound(NotFoundException exception) {
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Map.of(
-                        "status", HttpStatus.NOT_FOUND.value(),
-                        "error", "Not Found",
-                        "message", exception.getMessage()));
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<Map<String, Object>> handleAccessDenied(AccessDeniedException exception) {
-            
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(Map.of(
-                        "status", HttpStatus.FORBIDDEN.value(),
-                        "error", "Forbidden",
-                        "message", exception.getMessage()));
-    }
-
-    // alemmat voi poistaa myöhemmin, jos tarpeettomia
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Object>> RestArgumentNotValid(MethodArgumentNotValidException exception) {
+        @ExceptionHandler(BadRequestException.class)
+        public ResponseEntity<Map<String, Object>> RestBadRequest(BadRequestException exception) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Map.of(
-                        "status", HttpStatus.BAD_REQUEST.value(),
-                        "error", "Bad Request",
-                        "message", exception.getMessage()));
-    }
+                                .body(Map.of(
+                                                "status", HttpStatus.BAD_REQUEST.value(),
+                                                "error", "Bad Request",
+                                                "message", exception.getMessage()));
 
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Map<String, Object>> RestArgumentMismatch(MethodArgumentTypeMismatchException exception) {
+        }
+
+        @ExceptionHandler(NotFoundException.class)
+        public ResponseEntity<Map<String, Object>> RestNotFound(NotFoundException exception) {
+
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                .body(Map.of(
+                                                "status", HttpStatus.NOT_FOUND.value(),
+                                                "error", "Not Found",
+                                                "message", exception.getMessage()));
+        }
+
+        @ExceptionHandler(AccessDeniedException.class)
+        public ResponseEntity<Map<String, Object>> handleAccessDenied(AccessDeniedException exception) {
+
+                return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                                .body(Map.of(
+                                                "status", HttpStatus.FORBIDDEN.value(),
+                                                "error", "Forbidden",
+                                                "message", exception.getMessage()));
+        }
+
+        @ExceptionHandler(TicketAlreadyRedeemedException.class)
+        public ResponseEntity<Map<String, Object>> handleTicketAlreadyRedeemed(TicketAlreadyRedeemedException exception) {
+                return ResponseEntity.status(HttpStatus.CONFLICT)
+                                .body(Map.of(
+                                                "status", HttpStatus.CONFLICT.value(),
+                                                "error", "Already used",
+                                                "message", exception.getMessage()));
+        }
+
+        // alemmat voi poistaa myöhemmin, jos tarpeettomia
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<Map<String, Object>> RestArgumentNotValid(MethodArgumentNotValidException exception) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Map.of(
-                        "status", HttpStatus.BAD_REQUEST.value(),
-                        "error", "Bad Request",
-                        "message", exception.getMessage()));
-    }
+                                .body(Map.of(
+                                                "status", HttpStatus.BAD_REQUEST.value(),
+                                                "error", "Bad Request",
+                                                "message", exception.getMessage()));
+        }
+
+        @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+        public ResponseEntity<Map<String, Object>> RestArgumentMismatch(MethodArgumentTypeMismatchException exception) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                .body(Map.of(
+                                                "status", HttpStatus.BAD_REQUEST.value(),
+                                                "error", "Bad Request",
+                                                "message", exception.getMessage()));
+        }
 
 }
