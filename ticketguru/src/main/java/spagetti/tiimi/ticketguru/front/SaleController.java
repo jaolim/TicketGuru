346b08d06d018c15be0a3c504f526b18065f455e
Janne <bhi256@myy.haaga-helia.fi>
2025-11-17 15:16:29 +0200

    Front controller kansio lisätty

---
+++ ticketguru/src/main/java/spagetti/tiimi/ticketguru/front/SaleController.java
@@ -0,0 +1,33 @@
+package spagetti.tiimi.ticketguru.front;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import spagetti.tiimi.ticketguru.domain.CostRepository;
+import spagetti.tiimi.ticketguru.domain.EventRepository;
+import spagetti.tiimi.ticketguru.domain.SaleRepository;
+
+@Controller
+public class SaleController {
+
+    private EventRepository eRepository;
+    private SaleRepository sRepository;
+    private CostRepository cRepository;
+    
+    public SaleController(EventRepository eRepository, SaleRepository sRepository, CostRepository cRepository) {
+        this.eRepository = eRepository;
+        this.sRepository = sRepository;
+        this.cRepository = cRepository;
+    }
+
+    @GetMapping(value = { "/sell" })
+    public String getSell(Model model) {
+        model.addAttribute("events", eRepository.findAll());
+        model.addAttribute("costs", cRepository.findAll());
+        return "sell-ticket";
+    }
+
+
+
+}
