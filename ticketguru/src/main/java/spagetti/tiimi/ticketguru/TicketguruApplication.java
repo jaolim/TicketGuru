937179a0770db47c16ebb758c7bb16beec30c494
Janne <bhi256@myy.haaga-helia.fi>
2025-11-30 13:52:05 +0200

    Testidatan lisäysjärjestystä muutettu

--- ticketguru/src/main/java/spagetti/tiimi/ticketguru/TicketguruApplication.java
+++ ticketguru/src/main/java/spagetti/tiimi/ticketguru/TicketguruApplication.java
@@ -132,9 +132,9 @@ public class TicketguruApplication {
 				urepository.save(testUser);
 
 				Long savedEvent1Id = erepository.save(event1).getEventid();
+				Long savedEvent4Id = erepository.save(event4).getEventid();
 				Long savedEvent2Id = erepository.save(event2).getEventid();
 				Long savedEvent3Id = erepository.save(event3).getEventid();
-				Long savedEvent4Id = erepository.save(event4).getEventid();
 
 				crepository.save(cost1);
 				crepository.save(cost2);
