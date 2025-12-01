c1ed5931e554fe32f5cf595182493daf79885602
Janne <bhi256@myy.haaga-helia.fi>
2025-12-01 17:15:20 +0200

    Event crud tests

--- playwright_tests/tests/ticketguru_use.spec.ts
+++ playwright_tests/tests/ticketguru_use.spec.ts
@@ -4,13 +4,13 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
- let page: Page;
+let page: Page;
+const url = String(process.env.URL);
 
- //Login for tests session
- test.beforeAll(async ({ browser}) => {
+//Login for tests session
+test.beforeAll(async ({ browser }) => {
     const context = await browser.newContext();
     page = await context.newPage();
-    const url = String(process.env.URL);
 
     //homepage
     await page.goto(url);
@@ -22,15 +22,15 @@ dotenv.config();
     await page.getByLabel('password').fill(String(process.env.PASSWORD1));
     await page.getByRole('button', { name: 'Sign in' }).click();
 
- })
+})
 
 //Venues add, edit and deletion
-test('Venues CRUD', async ( ) => {
+test('Venues CRUD', async () => {
     const url = String(process.env.URL);
 
     //homepage
     await page.goto(url);
-    
+
     //Venues page
     await page.getByRole('link', { name: 'Venues' }).click();
     await expect(page).toHaveURL(`${url}/venuepage`);
@@ -82,4 +82,76 @@ test('Venues CRUD', async ( ) => {
     await expect(page.getByText('Playwright')).toHaveCount(0);
     await expect(page.getByText('Qwerty321')).toHaveCount(0);
 
+})
+
+//Events add, edit and deletion
+test('Events CRUD', async () => {
+    const url = String(process.env.URL);
+
+    //homepage
+    await page.goto(url);
+
+    //Events page
+    await page.getByRole('link', { name: 'Events' }).click();
+    await expect(page).toHaveURL(`${url}/eventpage`);
+
+    //Clearing leftovers
+    page.once('dialog', async dialog => {
+        await dialog.accept();
+    });
+    await trClickButtonIfExists(page, 'Playwright', 'Delete');
+    await trClickButtonIfExists(page, 'Qwerty321', 'Delete');
+
+    //Add event page
+    await page.getByRole('link', { name: 'Add New Event' }).click();
+    await expect(page).toHaveURL(`${url}/event/add`);
+
+    //Partial fill not accepted
+    const date = new Date().toISOString().slice(0, 16);
+    const button = page.getByRole('button', { name: 'save' });
+    await page.getByLabel('name').fill('Playwright');
+    await button.click()
+    await expect(page).toHaveURL(`${url}/event/add`);
+    await page.getByLabel('date').fill(date);
+    await button.click()
+    await expect(page).toHaveURL(`${url}/event/add`);
+    await page.locator('#venueId').selectOption({ label: 'Kulttuuritalo' });
+    await button.click()
+    await expect(page).toHaveURL(`${url}/event/add`);
+
+    //Full fill accepted
+    await page.getByLabel('capacity').fill('100');
+    await button.click()
+    await expect(page).toHaveURL(`${url}/eventpage`);
+
+    //Details visible
+    await page
+        .locator('tr', { has: page.getByText('Playwright') })
+        .getByRole('link', { name: 'Details' })
+        .click();
+    await expect(page.getByText('Event details for Playwright')).toBeVisible();
+    await page.goto(`${url}/eventpage`);
+
+    //Edit works
+    await page
+        .locator('tr', { has: page.getByText('Playwright') })
+        .getByRole('link', { name: 'Edit' })
+        .click();
+    await page.getByLabel('name').fill('Qwerty321');
+    await page.getByRole('button', { name: 'save' }).click();
+    await expect(page.getByText('Qwerty321')).toBeVisible();
+
+    //Delete element
+    const del = page
+        .locator('tr', { has: page.getByText('Qwerty321') })
+        .getByRole('button', { name: 'Delete' })
+    await expect(del).toHaveCount(1);
+
+    //Delete works
+    page.once('dialog', async dialog => {
+        await dialog.accept();
+    });
+    await del.click();
+    await expect(page.getByText('Playwright')).toHaveCount(0);
+    await expect(page.getByText('Qwerty321')).toHaveCount(0);
 })
